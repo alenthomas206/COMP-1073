af7ec9f2e95aed7fea17c5af548fc99cde9b0cb0
alenthomas206 <alenthomas206@gmail.com>
2025-10-19 20:13:59 -0400

    Added DOM element references for sliders and page background

---
+++ lab-2/js/page-color-changer.js
@@ -0,0 +1,5 @@
+//references to sliders and the HTML element
+const redSlider = document.getElementById('red');
+const greenSlider = document.getElementById('green');
+const blueSlider = document.getElementById('blue');
+const page = document.documentElement;
