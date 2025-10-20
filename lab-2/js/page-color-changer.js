fc4df3d8d6f946f816fda37da87028ce04ec92d6
alenthomas206 <alenthomas206@gmail.com>
2025-10-19 20:18:56 -0400

    created function to change bg colour

--- lab-2/js/page-color-changer.js
+++ lab-2/js/page-color-changer.js
@@ -3,3 +3,11 @@ const redSlider = document.getElementById('red');
 const greenSlider = document.getElementById('green');
 const blueSlider = document.getElementById('blue');
 const page = document.documentElement;
+
+// Function to change the background colour based on slider values
+function updateBackgroundColour() {
+    const r = redSlider.value;
+    const g = greenSlider.value;
+    const b = blueSlider.value;
+    page.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
