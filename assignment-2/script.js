1e155aa36b08de959d678d060f45fc313cf4eada
alenthomas206 <alenthomas206@gmail.com>
2025-11-14 16:05:00 -0500

    js to capture selected ingredients

--- assignment-2/script.js
+++ assignment-2/script.js
@@ -2,9 +2,11 @@
 document.getElementById("orderBtn").addEventListener("click", () => {
     const base = document.getElementById("base").value;// Capture base value 
     const size = document.getElementById("size").value;// Capture size value 
-
+    const selectedIngredients = [...document.querySelectorAll(".ingredients input:checked")]
+        .map(item => item.value);// Capture selected ingredients
     console.log("Base:", base);
     console.log("Size:", size);
+    console.log("Ingredients:", selectedIngredients);
 });
 
 
