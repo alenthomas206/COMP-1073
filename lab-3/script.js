601324391d060cece0ba776d9afcdf8d5237d4f7
alenthomas206 <alenthomas206@gmail.com>
2025-11-16 23:27:53 -0500

    toycar constructor

---
+++ lab-3/script.js
@@ -0,0 +1,8 @@
+function ToyCar(productName, brand, carBrand, carModel, scale, color) {
+    this.productName = productName;
+    this.brand = brand;
+    this.carBrand = carBrand;
+    this.carModel = carModel;
+    this.scale = scale;
+    this.color = color;
+}
