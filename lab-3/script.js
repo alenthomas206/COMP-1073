3faa996e1688587a54ba237f9d5c9b37a8f8f36d
alenthomas206 <alenthomas206@gmail.com>
2025-11-16 23:29:44 -0500

    additional properties

--- lab-3/script.js
+++ lab-3/script.js
@@ -1,8 +1,15 @@
-function ToyCar(productName, brand, carBrand, carModel, scale, color) {
+function ToyCar(productName, brand, carBrand, carModel, scale, color, material, length, width, height, packaging, sku, upc) {
     this.productName = productName;
     this.brand = brand;
     this.carBrand = carBrand;
     this.carModel = carModel;
     this.scale = scale;
     this.color = color;
+    this.material = material;
+    this.length = length;
+    this.width = width;
+    this.height = height;
+    this.packaging = packaging;
+    this.sku = sku;
+    this.upc = upc;
 }
