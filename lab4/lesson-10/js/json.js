a35de8fc401372466ad5634bfa2bc30f08ed8973
alenthomas206 <alenthomas206@gmail.com>
2025-11-23 23:39:30 -0500

    function to fetch json file

--- lab4/lesson-10/js/json.js
+++ lab4/lesson-10/js/json.js
@@ -3,27 +3,29 @@ const header = document.querySelector('header');
 const section = document.querySelector('section');
 
 
-// STEP 3a: Create the asynchronous function populate()
 
+// STEP 3a: Create the asynchronous function populate()
+async function populate() {
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4: Store the URL of a JSON file in a variable */
-    
+    const requestURL = './i-scream.json';
     // STEP 5: Use the new URL to create a new request object
     
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
-    
+    const response = await fetch(requestURL);
     // STEP 7: Capture the returned Response object and covert to a JSON object using json()
-    
+    const jsonObj = await response.json();
     // STEP 8: Output the iScream JSON object to the console 
-    
+    console.log(jsonObj);
     // STEP 9a: Invoke the populateHeader function here, then build it below
-    
+    populateHeader(jsonObj);
     // STEP 10a: Invoke the showTopFlavors function here, then build it below
-    
+    showTopFlavors(jsonObj);
+}
 
 
 // STEP 3b: Call the populate() function
-
+populate();
 
 /* STEP 9b: Build out the populateHeader() function */
 function populateHeader() {
